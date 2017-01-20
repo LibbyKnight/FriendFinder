b4579f79a3896f2506c2b790fe1fd34f67478e38
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-01-20 15:30:03 -0500

    fixed routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,18 +2,12 @@ var friend = require('../data/friends.js');
 
 var path = require('path');
 
-
+	userInput = []
 
 module.exports = function(app){
-	for (i = 0; i < friendArray.length; i++) {
-		
-		var friend = friendArray[i].scores;
-		var user = userArray.scores;
-		
-		for (i = 0; i < friend; i++) {
-
-			
-		}
-	}
 	
-};
+
+
+
+	};
+
