ae1719a595f7f26c36d000043061cacfd2c0364e
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-01-20 18:31:40 -0500

    testing user input

--- server.js
+++ server.js
@@ -15,6 +15,40 @@ require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 
+
+var Data = [];
+
+
+
+app.get('/api/currentData', function(req, res) {
+   var curData = [];
+   if (Data.length == 4) {
+       res.json(Data);
+   } else {
+       for (var i = 0; i < Data.length; i++) {
+           curData.push(Data[i])
+       }
+
+       res.json(curData);
+   }
+
+});
+
+app.get('/api/tables', function(req, res) {
+   res.json(Data);
+});
+
+app.post('/api/new', function(req, res) {
+   var userData = req.body;
+   Data.push(userData);
+
+   console.log(userData);
+
+   res.json(Data);
+})
+
+
+
 app.listen(PORT, function() {
   console.log("App listening.");
 });
